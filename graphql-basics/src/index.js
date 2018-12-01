85c9d6ea97975e2a75b61072bb3360d5aab6f475
cyberinfoscripter <007arunwilson@gmail.com>
2018-12-01 11:43:23 -0500

    Added Graphql Server Schema ( type Defs ) Resolvers

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,31 @@
-console.log("GraphQl with yoga");
+import { GraphQLServer } from "graphql-yoga";
+
+//Type Definitions
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+
+//Resolvers
+const resolvers = {
+  Query: {
+    hello: () => "Hello, welcome to graphQl",
+    name: () => "Arun Wilson",
+    location: () => "TVM, Kerala - India",
+    bio: () =>
+      "Programmer with proficiency in Javascript & PHP, Currently Focused in React and React Native with Devops and AWS"
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("GraphQl Server start");
+});
